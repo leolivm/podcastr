b107123261cc9446deba5159af81fb23313c7a4c
leolivm <leolivm@outlook.com>
2021-09-27 23:21:31 -0300

    initializing episode page

---
+++ src/pages/episodes/[slug].tsx
@@ -0,0 +1,63 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { parseISO, format } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import api from '../../services/api'
+import convertDurationToTimeString from '../../utils/convertDurationToTimeString'
+
+interface Episode {
+  id: string
+  title: string
+  thumbnail: string
+  members: string
+  duration: number
+  durationAsString: string
+  description: string
+  url: string
+  publishedAt: string
+}
+
+interface EpisodeProps {
+  episode: Episode
+}
+
+const Episode = ({ episode }: EpisodeProps) => {
+  return (
+    <div>
+      <h1>{episode.title}</h1>
+    </div>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ctx => {
+  const { slug } = ctx.params
+  const { data } = await api.get(`/episodes/${slug}`)
+
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', {
+      locale: ptBR,
+    }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  }
+
+  return {
+    props: {
+      episode,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  }
+}
+
+export default Episode
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next'
 import Image from 'next/image'
+import Link from 'next/link'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import api from '../services/api'
@@ -10,7 +11,6 @@ interface EpisodeProps {
   id: string
   title: string
   thumbnail: string
-  description: string
   members: string
   duration: number
   durationAsString: string
@@ -58,7 +58,9 @@ const Home = ({ latestEpisodes, allEpisodes }: Homeprops) => {
               />
 
               <div className={styles.episodeDetails}>
-                <a href="/">{episode.title}</a>
+                <Link href={`/episodes/${episode.id}`}>
+                  <a>{episode.title}</a>
+                </Link>
                 <p>{episode.members}</p>
                 <span>{episode.publishedAt}</span>
                 <span>{episode.durationAsString}</span>
@@ -77,12 +79,14 @@ const Home = ({ latestEpisodes, allEpisodes }: Homeprops) => {
 
         <table cellSpacing={0}>
           <thead>
-            <th />
-            <th>Podcast</th>
-            <th>Integrantes</th>
-            <th>Data</th>
-            <th>Duração</th>
-            <th />
+            <tr>
+              <th />
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th />
+            </tr>
           </thead>
           <tbody>
             {allEpisodes.map(episode => (
@@ -97,7 +101,9 @@ const Home = ({ latestEpisodes, allEpisodes }: Homeprops) => {
                   />
                 </td>
                 <td>
-                  <a href="/">{episode.title}</a>
+                  <Link href={`/episodes/${episode.id}`} prefetch>
+                    <a>{episode.title}</a>
+                  </Link>
                 </td>
                 <td>{episode.members}</td>
                 <td style={{ width: 100 }}>{episode.publishedAt}</td>
@@ -138,7 +144,6 @@ export const getStaticProps: GetStaticProps = async () => {
       durationAsString: convertDurationToTimeString(
         Number(episode.file.duration)
       ),
-      description: episode.description,
       url: episode.file.url,
     }
   })
