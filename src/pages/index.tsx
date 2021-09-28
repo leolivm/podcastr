4a7b5ae60bab9f7ac2818419e8b0da79263567e2
leolivm <leolivm@outlook.com>
2021-09-27 22:27:13 -0300

    styling homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,119 @@
 import { GetStaticProps } from 'next'
+import Image from 'next/image'
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import api from '../services/api'
 import convertDurationToTimeString from '../utils/convertDurationToTimeString'
+import styles from './home.module.scss'
 
 interface EpisodeProps {
   id: string
   title: string
+  thumbnail: string
+  description: string
   members: string
-  published_at: string
+  duration: number
+  durationAsString: string
+  url: string
+  publishedAt: string
 }
 
 interface Homeprops {
-  episodes: EpisodeProps[]
+  latestEpisodes: EpisodeProps[]
+  allEpisodes: EpisodeProps[]
+}
+
+interface DataEpisode {
+  id: string
+  title: string
+  thumbnail: string
+  members: string
+  published_at: string
+  file: {
+    duration: number
+    url: string
+  }
+  description: string
 }
 
 // Client-side SPA
 // Server-side SSR
 // Static Site Generation SSG
 
-const Home = (props: Homeprops) => {
-  const { episodes } = props
+const Home = ({ latestEpisodes, allEpisodes }: Homeprops) => {
+  return (
+    <div className={styles.homepage}>
+      <section className={styles.latestEpisodes}>
+        <h1>Últimos lançamentos</h1>
 
-  return <div>{JSON.stringify(episodes)}</div>
+        <ul>
+          {latestEpisodes.map(episode => (
+            <li key={episode.id}>
+              <Image
+                width={192}
+                height={192}
+                src={episode.thumbnail}
+                alt={episode.title}
+                objectFit="cover"
+              />
+
+              <div className={styles.episodeDetails}>
+                <a href="/">{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.durationAsString}</span>
+              </div>
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <th />
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th />
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => (
+              <tr key={episode.id}>
+                <td style={{ width: 72 }}>
+                  <Image
+                    width={120}
+                    height={120}
+                    src={episode.thumbnail}
+                    alt={episode.title}
+                    objectFit="cover"
+                  />
+                </td>
+                <td>
+                  <a href="/">{episode.title}</a>
+                </td>
+                <td>{episode.members}</td>
+                <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                <td>{episode.durationAsString}</td>
+                <td>
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episódio" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
@@ -34,7 +125,7 @@ export const getStaticProps: GetStaticProps = async () => {
     },
   })
 
-  const episodes = data.map(episode => {
+  const episodes = data.map((episode: DataEpisode) => {
     return {
       id: episode.id,
       title: episode.title,
@@ -52,9 +143,13 @@ export const getStaticProps: GetStaticProps = async () => {
     }
   })
 
+  const latestEpisodes = episodes.slice(0, 2)
+  const allEpisodes = episodes.slice(2, episodes.length)
+
   return {
     props: {
-      episodes,
+      latestEpisodes,
+      allEpisodes,
     },
     revalidate: 60 * 60 * 8, // 24 hours
   }
