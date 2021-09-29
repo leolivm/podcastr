7ca64e6b9a9651a19aa90b0e0ad20b01ecdd5022
leolivm <leolivm@outlook.com>
2021-09-28 23:23:27 -0300

    context init

---
+++ src/contexts/PlayerContext.ts
@@ -0,0 +1,5 @@
+import { createContext } from 'react'
+
+const PlayerContext = createContext('Leandro')
+
+export default PlayerContext
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,17 +3,20 @@ import Header from '../components/Header'
 import Player from '../components/Player'
 import '../styles/global.scss'
 import styles from '../styles/app.module.scss'
+import PlayerContext from '../contexts/PlayerContext'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
+    <PlayerContext.Provider value="asd">
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
 
-      <Player />
-    </div>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
